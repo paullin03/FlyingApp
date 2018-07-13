fa610592ac1c5b0888139512b8e835bfa2f867b9
Paul <you@example.com>
2018-07-13 23:30:00 +0800

    feat: check center of gravity limits Prints center of gravity category in addition to displaying emptyPlane and takeoff statistics

--- app/src/main/java/com/example/android/flyingapp/CheckWeight.java
+++ app/src/main/java/com/example/android/flyingapp/CheckWeight.java
@@ -1,5 +1,7 @@
 package com.example.android.flyingapp;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,14 @@ public class CheckWeight extends AppCompatActivity {
     final int PASSENGER_ARM = 73;
     final int FUEL_ARM = 48;
     final int BAGGAGE_ARM = 95;
+    final int LOWER_CG_LIMIT = 35;
+    final double UPPER_CG_LIMIT = 47.4;
+    final int LOWER_WEIGHT_LIMIT = 1560;
+    final int UPPER_WEIGHT_LIMIT = 2450;
+    final int CG_INTERSECTION = 40;
+    final int WEIGHT_INTERSECTION = 1950;
+    final double UTILITY_CG_LIMIT = 40.5;
+    final int UTILITY_WEIGHT_LIMIT = 2100;
 
     EditText emptyWeight;
     EditText emptyArm;
@@ -26,9 +36,12 @@ public class CheckWeight extends AppCompatActivity {
     TextView emptyPlaneWeight;
     TextView emptyPlaneArm;
     TextView emptyPlaneMoment;
+    TextView emptyPlaneRegion;
     TextView takeoffWeight;
     TextView takeoffArm;
     TextView takeoffMoment;
+    TextView takeoffRegion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +60,11 @@ public class CheckWeight extends AppCompatActivity {
         emptyPlaneWeight=findViewById(R.id.empty_plane_weight);
         emptyPlaneArm=findViewById(R.id.empty_plane_arm);
         emptyPlaneMoment=findViewById(R.id.empty_plane_moment);
+        emptyPlaneRegion=findViewById(R.id.empty_plane_region);
         takeoffWeight=findViewById(R.id.takeoff_weight);
         takeoffArm=findViewById(R.id.takeoff_arm);
         takeoffMoment=findViewById(R.id.takeoff_moment);
+        takeoffRegion=findViewById(R.id.takeoff_region);
     }
 
     public void calculate(View view){
@@ -77,6 +92,40 @@ public class CheckWeight extends AppCompatActivity {
         takeoffWeight.setText(String.format("%.2f",weightTakeoff));
         takeoffArm.setText(String.format("%.2f",armTakeoff));
         takeoffMoment.setText(String.format("%.2f",momentTakeoff));
+
+        if(armEmptyPlane<=LOWER_CG_LIMIT||armEmptyPlane>=UPPER_CG_LIMIT||
+                weightEmptyPlane<=LOWER_WEIGHT_LIMIT||weightEmptyPlane>=UPPER_WEIGHT_LIMIT||
+                (weightEmptyPlane-WEIGHT_INTERSECTION>=
+                        ((armEmptyPlane-LOWER_CG_LIMIT)*((UPPER_WEIGHT_LIMIT-WEIGHT_INTERSECTION)/(CG_INTERSECTION-LOWER_CG_LIMIT))))){
+            emptyPlaneRegion.setText(R.string.out_of_bounds);
+            emptyPlaneRegion.setTextColor(getResources().getColor(R.color.colorOutOfBounds));
+        }
+        else if(armEmptyPlane<UTILITY_CG_LIMIT&&weightEmptyPlane<UTILITY_WEIGHT_LIMIT){
+            emptyPlaneRegion.setText(R.string.utility);
+            emptyPlaneRegion.setTextColor(getResources().getColor(R.color.colorUtility));
+        }
+        else{
+            emptyPlaneRegion.setText(R.string.normal);
+            emptyPlaneRegion.setTextColor(getResources().getColor(R.color.colorNormal));
+        }
+
+        if(armTakeoff<=LOWER_CG_LIMIT||armTakeoff>=UPPER_CG_LIMIT||
+                weightTakeoff<=LOWER_WEIGHT_LIMIT||weightTakeoff>=UPPER_WEIGHT_LIMIT||
+                (weightTakeoff-WEIGHT_INTERSECTION>=
+                        ((armTakeoff-LOWER_CG_LIMIT)*((UPPER_WEIGHT_LIMIT-WEIGHT_INTERSECTION)/(CG_INTERSECTION-LOWER_CG_LIMIT))))){
+            takeoffRegion.setText(R.string.out_of_bounds);
+            takeoffRegion.setTextColor(getResources().getColor(R.color.colorOutOfBounds));
+        }
+        else if(armTakeoff<UTILITY_CG_LIMIT&&weightTakeoff<UTILITY_WEIGHT_LIMIT){
+            takeoffRegion.setText(R.string.utility);
+            takeoffRegion.setTextColor(getResources().getColor(R.color.colorUtility));
+        }
+        else{
+            takeoffRegion.setText(R.string.normal);
+            takeoffRegion.setTextColor(getResources().getColor(R.color.colorNormal));
+        }
+
+
     }
 
 }
